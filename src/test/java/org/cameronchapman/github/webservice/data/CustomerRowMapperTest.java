90f5d375979345f598a04650607ed7d82ea6515e
Cameron Chapman <cameron.chapman@garmin.com>
2017-01-14 13:19:19 -0600

    Fixing merge issue.

--- src/test/java/org/cameronchapman/github/webservice/data/CustomerRowMapperTest.java
+++ src/test/java/org/cameronchapman/github/webservice/data/CustomerRowMapperTest.java
@@ -14,31 +14,18 @@ import org.mockito.runners.MockitoJUnitRunner;
 public class CustomerRowMapperTest {
     
 	@Test
-<<<<<<< HEAD
-	public void mapRowTest() throws Exception {
-		CustomerRowMapper customerRowMapper = new CustomerRowMapper();
-		ResultSet rs = Mockito.mock(ResultSet.class);
-		Long id = new Long(1);
-=======
 	public void customerRowMapperTest() throws Exception {
 		CustomerRowMapper customerRowMapper = new CustomerRowMapper();
 		Long id = new Long(1);
 		ResultSet rs = Mockito.mock(ResultSet.class);
->>>>>>> Removing snapshot, adding untracked directory.
 		Mockito.when(rs.getLong("id")).thenReturn(id);
 		Mockito.when(rs.getString("name")).thenReturn("Customer Name");
 		Mockito.when(rs.getString("address1")).thenReturn("Address 1");
 		Mockito.when(rs.getString("address2")).thenReturn("Address 2");
 		Mockito.when(rs.getString("city")).thenReturn("City");
 		Mockito.when(rs.getString("state")).thenReturn("State");
-		Mockito.when(rs.getString("zip")).thenReturn("Zip");
-<<<<<<< HEAD
+		Mockito.when(rs.getString("zip")).thenReturn("Zip");		
 		Customer customer = customerRowMapper.mapRow(rs, 1);
-=======
-		
-		Customer customer = customerRowMapper.mapRow(rs, 1);
-		
->>>>>>> Removing snapshot, adding untracked directory.
 		assertEquals(customer.getId(), id);
 		assertEquals(customer.getName(), "Customer Name");
 		assertEquals(customer.getAddress1(), "Address 1");
@@ -47,9 +34,5 @@ public class CustomerRowMapperTest {
 		assertEquals(customer.getState(), "State");
 		assertEquals(customer.getZip(), "Zip");
 	}
-<<<<<<< HEAD
-}
-=======
 	
 }
->>>>>>> Removing snapshot, adding untracked directory.
